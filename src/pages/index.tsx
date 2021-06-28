62c8f4d71bd4384b8c49ed5c981dc121c7c292e9
y4shiro <y4shironao@gmail.com>
2021-06-28 16:18:18 +0900

    大まかな構成追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,31 @@
-import React from "react"
+import React from 'react';
 import * as styles from './index.module.css';
 
-// markup
 const IndexPage = () => {
   return (
     <>
-      <h1 className={styles.title}>Hello</h1>
-      <p className={styles.text}>text</p>
+      <header className={styles.header}>
+        <h1>タイトルロゴ</h1>
+        <nav>
+          <ul>
+            <li>メニュー</li>
+            <li>メニュー</li>
+            <li>メニュー</li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <section>メインビジュアル</section>
+        <section>
+          <h1 className={styles.title}>Hello</h1>
+          <p className={styles.text}>text</p>
+        </section>
+      </main>
+      <footer>
+        <p>&copy; 2021 y4shiro</p>
+      </footer>
     </>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
