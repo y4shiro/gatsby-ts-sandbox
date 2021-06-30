61dffb39e00302b0ff5347a31f05235281fe7ede
y4shiro <y4shironao@gmail.com>
2021-06-30 16:50:31 +0900

    Works コンポーネントに分離

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import * as styles from './index.module.css';
 import 'normalize.css';
 
-import { StaticImage } from 'gatsby-plugin-image';
-
 import Header from '../components/Header';
 import MainVisual from '../components/MainVisual';
 import About from '../components/About';
+import Works from '../components/Works';
 
 const IndexPage = () => {
   return (
@@ -15,14 +14,7 @@ const IndexPage = () => {
       <main>
         <MainVisual />
         <About />
-
-        <section className={styles.contentLayout}>
-          <h1 className={styles.contentTitle}>Works</h1>
-          <p>
-            　吾輩わがはいは猫である。名前はまだ無い。
-            　どこで生れたかとんと見当けんとうがつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。吾輩はここで始めて人間というものを見た。しかもあとで聞くとそれは書生という人間中で一番獰悪どうあくな種族であったそうだ。この書生というのは時々我々を捕つかまえて煮にて食うという話である。しかしその当時は何という考もなかったから別段恐しいとも思わなかった。ただ彼の掌てのひらに載せられてスーと持ち上げられた時何だかフワフワした感じがあったばかりである。
-          </p>
-        </section>
+        <Works />
       </main>
       <footer className={styles.footerLayout}>
         <p className={styles.footerText}>&copy; 2021 y4shiro</p>
