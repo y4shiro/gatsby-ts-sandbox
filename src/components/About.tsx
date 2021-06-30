daf432a78a6592ef13d15fadcf21281dece39cd0
y4shiro <y4shironao@gmail.com>
2021-06-30 16:47:59 +0900

    About コンポーネントに分割

---
+++ src/components/About.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import * as styles from './Content.module.css';
+
+const About = () => {
+  return (
+    <section className={styles.contentLayout}>
+      <h1 className={styles.contentTitle}>About</h1>
+      <p>
+        　あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。
+        　またそのなかでいっしょになったたくさんのひとたち、ファゼーロとロザーロ、羊飼のミーロや、顔の赤いこどもたち、地主のテーモ、山猫博士のボーガント・デストゥパーゴなど、いまこの暗い巨きな石の建物のなかで考えていると、みんなむかし風のなつかしい青い幻燈のように思われます。では、わたくしはいつかの小さなみだしをつけながら、しずかにあの年のイーハトーヴォの五月から十月までを書きつけましょう。
+      </p>
+    </section>
+  );
+};
+
+export default About;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { StaticImage } from 'gatsby-plugin-image';
 
 import Header from '../components/Header';
 import MainVisual from '../components/MainVisual';
+import About from '../components/About';
 
 const IndexPage = () => {
   return (
@@ -13,14 +14,7 @@ const IndexPage = () => {
       <Header />
       <main>
         <MainVisual />
-
-        <section className={styles.contentLayout}>
-          <h1 className={styles.contentTitle}>About</h1>
-          <p>
-            　あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。
-            　またそのなかでいっしょになったたくさんのひとたち、ファゼーロとロザーロ、羊飼のミーロや、顔の赤いこどもたち、地主のテーモ、山猫博士のボーガント・デストゥパーゴなど、いまこの暗い巨きな石の建物のなかで考えていると、みんなむかし風のなつかしい青い幻燈のように思われます。では、わたくしはいつかの小さなみだしをつけながら、しずかにあの年のイーハトーヴォの五月から十月までを書きつけましょう。
-          </p>
-        </section>
+        <About />
 
         <section className={styles.contentLayout}>
           <h1 className={styles.contentTitle}>Works</h1>
