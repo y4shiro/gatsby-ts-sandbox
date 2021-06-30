ce109fc6ccb859e70f6d4d0dd9194ad04267afb2
y4shiro <y4shironao@gmail.com>
2021-06-30 16:53:44 +0900

    Footer コンポーネントに分割

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footerLayout}>
+      <p className={styles.footerText}>&copy; 2021 y4shiro</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import * as styles from './index.module.css';
 import 'normalize.css';
 
 import Header from '../components/Header';
 import MainVisual from '../components/MainVisual';
 import About from '../components/About';
 import Works from '../components/Works';
+import Footer from '../components/Footer';
 
 const IndexPage = () => {
   return (
@@ -16,9 +16,7 @@ const IndexPage = () => {
         <About />
         <Works />
       </main>
-      <footer className={styles.footerLayout}>
-        <p className={styles.footerText}>&copy; 2021 y4shiro</p>
-      </footer>
+      <Footer />
     </>
   );
 };
